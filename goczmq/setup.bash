13050edb4e9c23717dfcb9fa7dd35d63b7f60c12
Tianon Gravi <admwiggin@gmail.com>
2015-09-16 22:54:14 -0700

    Add "set -ex" for stopping the setup scripts on failing commands, chmod +x all the setup scripts, and comment out upstream test suites that we don't actually need to run

--- goczmq/setup.bash
+++ goczmq/setup.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 # setup steps taken from https://github.com/zeromq/goczmq
 
@@ -11,7 +12,7 @@ gpg --import jedi.gpg.asc
 gpg --verify libsodium-1.0.3.tar.gz.sig libsodium-1.0.3.tar.gz
 tar zxvf libsodium-1.0.3.tar.gz
 pushd libsodium-1.0.3
-./configure; make check
+./configure; #make check
 sudo make install
 sudo ldconfig
 popd
@@ -19,7 +20,7 @@ popd
 wget http://download.zeromq.org/zeromq-4.1.1.tar.gz
 tar zxvf zeromq-4.1.1.tar.gz
 pushd zeromq-4.1.1
-./configure --with-libsodium; make; make check
+./configure --with-libsodium; make; #make check
 sudo make install
 sudo ldconfig
 popd
@@ -27,7 +28,7 @@ popd
 wget http://download.zeromq.org/czmq-3.0.1.tar.gz
 tar zxvf czmq-3.0.1.tar.gz
 pushd czmq-3.0.1
-./configure; make check
+./configure; #make check
 sudo make install
 sudo ldconfig
 popd
