ebb71bdc959d6e6a5c95d8c94e6302a3b85d19b6
Dave Cheney <dave@cheney.net>
2016-05-26 16:23:12 +1000

    Replace sqlite3 example

--- sqlite3/src/sqlite/test.go
+++ sqlite3/src/sqlite/test.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"github.com/mattn/go-sqlite3"
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	c, _ := sqlite3.Open("sqlite.db")
-	fmt.Println("c is ", c)
+	os.Remove("./foo.db")
+
+	db, err := sql.Open("sqlite3", "./foo.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 }
