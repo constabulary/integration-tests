d617491e698e0eecb5da1a4940838ee93e42be49
Dave Cheney <dave@cheney.net>
2016-09-26 20:27:52 +1000

    Skip test on Go 1.4

--- mockery/run.bash
+++ mockery/run.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 
+if (~/go1.4/bin/go version 2>&1 | grep -o "go1.4") ; then
+	exit 0
+fi
+
 gb vendor fetch github.com/vektra/mockery
 install -d src/github.com/vektra/mockery/cmd/mockery
 install vendor/src/github.com/vektra/mockery/cmd/mockery/mockery.go src/github.com/vektra/mockery/cmd/mockery 
